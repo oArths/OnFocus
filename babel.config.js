85757eb38945398dc74fa8d53c9242a41d62f487
Arthur <132010817+oArths@users.noreply.github.com>
2024-02-05 01:06:02 -0300

    createTask
    adicionando funcionalidades como timer no createTask

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    sourceMaps: true,
     plugins: [
       '@babel/plugin-proposal-export-namespace-from',
       'react-native-reanimated/plugin',
